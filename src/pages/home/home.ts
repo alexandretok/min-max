9543728647cb096fe0250990e3c2106d9fd07843
Alexandre Justino <alexandretok@gmail.com>
2018-03-19 08:34:14 -0300

    Commit inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,52 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  interval;
+  valorJogador = 1;
+  valorComputador = -1;
+  jogadorDaVez = 0;
+  tabuleiro = [[0,0,0],[0,0,0],[0,0,0]];
 
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+    // Escolher quem inicia
+    let confirm = this.alertCtrl.create({
+      title: 'Quem vai iniciar?',
+      message: 'Escolha abaixo quem irá ter a primeira jogada?',
+      buttons: [
+        {
+          text: 'O computador',
+          handler: () => {
+            this.jogadorDaVez = this.valorComputador;
+            console.log(this.jogadorDaVez);
+          }
+        },
+        {
+          text: 'Eu',
+          handler: () => {
+            this.jogadorDaVez = this.valorJogador;
+            console.log(this.jogadorDaVez);
+          }
+        }
+      ]
+    });
+    confirm.present();
+
+    this.interval = setInterval(() => {
+      this.minMax();
+    }, 1000);
   }
 
+  click(i, j) {
+    if(this.jogadorDaVez == this.valorJogador) {
+      this.tabuleiro[i][j] = this.valorJogador;
+      this.jogadorDaVez = this.valorComputador;
+    }
+  }
+
+  minMax(){
+    if(this.jogadorDaVez == this.valorComputador){
+      this.tabuleiro[0][0] = this.valorComputador;
+      this.jogadorDaVez = this.valorJogador;
+    }
+  }
 }
