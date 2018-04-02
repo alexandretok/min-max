7b4972e2677572f7e7fb6aa7d91e63a3f497b89f
Alexandre Justino <alexandretok@gmail.com>
2018-04-02 08:16:39 -0300

    Add podeGanhar()

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -49,10 +49,59 @@ export class HomePage {
     }
   }
 
-  minMax(){
+  minMax() {
     if(this.jogadorDaVez == this.valorComputador){
-      this.tabuleiro[0][0] = this.valorComputador;
+      console.log(this.podeGanhar());
       this.jogadorDaVez = this.valorJogador;
     }
+  }
+
+  podeGanhar(){
+    let somaLinhas = new Array(this.tabuleiro.length+1).join('0').split('').map(parseFloat);
+    let somaColunas = new Array(this.tabuleiro[0].length+1).join('0').split('').map(parseFloat);
+    let somaDiagonalPrincipal = 0;
+    let somaDiagonalSecundaria = 0;
+
+    // Calcular
+    for(let i=0; i < this.tabuleiro.length; i++){
+      for(let j=0; j < this.tabuleiro[i].length; j++) {
+        somaLinhas[i] += this.tabuleiro[i][j];
+        somaColunas[j] += this.tabuleiro[i][j];
+        if(i == j) somaDiagonalPrincipal += this.tabuleiro[i][j];
+        if(i+j == 2) somaDiagonalSecundaria += this.tabuleiro[i][j];
+      }
+    }
+
+    // Verificar
+
+    if(somaDiagonalPrincipal == 2)
+      for(let i=0; i < this.tabuleiro.length; i++){
+        for(let j=0; j < this.tabuleiro[i].length; j++) {
+          if(i == j && this.tabuleiro[i][j] == 0) return [i, j];
+        }
+      }
+
+    if(somaDiagonalSecundaria == 2)
+      for(let i=0; i < this.tabuleiro.length; i++){
+        for(let j=0; j < this.tabuleiro[i].length; j++) {
+          if(i+j == 2 && this.tabuleiro[i][j] == 0) return [i, j];
+        }
+      }
+
+    for(let linha=0; linha < somaLinhas.length; linha++)
+      if(somaLinhas[linha] == 2)
+        for(let i=0; i < this.tabuleiro.length; i++){
+          for(let j=0; j < this.tabuleiro[i].length; j++) {
+            if(i == linha && this.tabuleiro[i][j] == 0) return [i, j];
+          }
+        }
+
+    for(let coluna=0; coluna < somaColunas.length; coluna++)
+      if(somaColunas[coluna] == 2)
+        for(let i=0; i < this.tabuleiro.length; i++){
+          for(let j=0; j < this.tabuleiro[i].length; j++) {
+            if(j == coluna && this.tabuleiro[i][j] == 0) return [i, j];
+          }
+        }
   }
 }
